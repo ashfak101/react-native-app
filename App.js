bd3984a550bb51a0ae9e40d1531c5a429940f522
ashfakul <ashfakulkarim2@gmail.com>
2023-01-03 20:59:24 +0600

    Color ,spacing and typography setup done

--- App.js
+++ App.js
@@ -3,19 +3,22 @@ import { StyleSheet, Text, View } from 'react-native'
 import { colors } from './src/theme/color'
 import { spacing } from './src/theme/spacing'
 import { useFonts } from 'expo-font'
+import { typography } from './src/theme/typography'
 export default function App() {
     const [loaded] = useFonts({
         'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
         'Antonio-Bold': require('./assets/fonts/Antonio-Bold.ttf'),
-        'LeagueSpartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf')
+        'LeagueSpartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf'),
+        'LeagueSpartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf')
     })
+
     if (!loaded) {
         return <Text>Text is Loading</Text>
     }
     return (
         <View style={styles.container}>
             <Text style={{
-                fontFamily:"Antonio-Medium"
+                fontFamily:typography.bold
             }}>Open up App.js to start working on your app!</Text>
             <Text style={{
                 marginTop: spacing[2],
