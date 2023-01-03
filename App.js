3af3b78bbd9ce91cefd45603d6b7f78a6c33c640
ashfakul <ashfakulkarim2@gmail.com>
2023-01-03 20:34:26 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { colors } from './src/theme/color'
+import { spacing } from './src/theme/spacing'
+import { useFonts } from 'expo-font'
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [loaded] = useFonts({
+        'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+        'Antonio-Bold': require('./assets/fonts/Antonio-Bold.ttf'),
+        'LeagueSpartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf')
+    })
+    if (!loaded) {
+        return <Text>Text is Loading</Text>
+    }
+    return (
+        <View style={styles.container}>
+            <Text style={{
+                fontFamily:"Antonio-Medium"
+            }}>Open up App.js to start working on your app!</Text>
+            <Text style={{
+                marginTop: spacing[2],
+                fontFamily:"LeagueSpartan-Regular"
+            }}>This is my first react native app</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: colors.green,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
---
+++ src/theme/color.js
@@ -0,0 +1,13 @@
+export const colors ={
+    black:'#070724',
+    white:'#fff',
+    darkGrey:'#38384F',
+    grey:'#838391',
+    lightGreen:'#419E88',
+    orange:'#EDA249',
+    purple:'#6f2ed6',
+    darkOrange:'#D14C32',
+    red:'#D83A34',
+    lightRed:'#CD5120',
+    green:'#1ec2a4'
+}
